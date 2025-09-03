b75097af86e14028c646eb40734329557900b889
Ranj4ni <ranjani.r4j@gmail.com>
2025-09-03 14:24:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "web" {
         Name = "web-${terraform.workspace}" # Name = "dev-${count.index}"
     }
 }
+# simple  creation of ec2
+resource "aws_instance" "web" {
+    ami = "ami-0d6b57555f03e6be8"
+    instance_type = "t2.small"
+    count = 5
+    tags = {
+        Name = "terraform-ec2"
+    }
+}

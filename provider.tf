125721d9c2d1c2588b12abaffd85503a0ceb6dfe
Ranj4ni <ranjani.r4j@gmail.com>
2025-09-03 14:04:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,11 @@
-resource "aws_instance" "web" {
-    ami = "ami-06e11c4cc68c362dd"
-    instance_type = "t2.micro"
-    #count = 2                                          
-    tags = {                                 # workspace names = default & app          
-        Name = "web-${terraform.workspace}" # Name = "dev-${count.index}"
-    }
+provider "aws" {
+  region     = "us-west-1"
+}
+# create remote backend of s3
+terraform {
+  backend "s3" {
+    bucket         = "terraform-dancing-duck"
+    key            = "ranjani/terraform.tfstate"
+    region        = "us-west-1"
+  }
 }
